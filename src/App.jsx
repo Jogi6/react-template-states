719a03ef668a97c92eb45c9259ba57ea83ea9b92
hyperv <hyperv@ucione.local>
2023-02-07 21:03:54 +0100

    FirstCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Component } from "react";
+import Show from "./show";
 
-const App = () => {
-  return (
-    <div></div>
-  );
+class App extends Component{
+  state = {
+    count: 0
+  }
+  
+  setState({count=this.state.count +1})
+
+  render() {
+    return <div><Show count={this.state.count} /></div>;
+  }
 }
 
 export default App;
---
+++ src/Show.jsx
@@ -0,0 +1,5 @@
+const Show = ({count}) => {
+    return <hi>Count: {count}</hi>
+}
+
+export default Show;
