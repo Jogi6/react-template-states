aac4f39fb696cbe42fb120fa2a0d884514b893e8
hyperv <hyperv@ucione.local>
2023-02-07 21:23:01 +0100

    Gotovo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { Component } from "react";
-import Show from "./show";
+import Show from "./Show";
 
 class App extends Component{
-  state = {
-    count: 0
+  constructor(){
+    super();
+
+    this.state = {
+      count: 0
+    };
+
+    //Ispravno je pozvati u metodi componentDidMount()
+    setInterval(() => {
+      this.setState({count: this.state.count + 1});
+      // this.state.count = this.state.count + 1; Ovo ne raditi!!!
+    }, 1000);
   }
-  
-  setState({count=this.state.count +1})
 
   render() {
-    return <div><Show count={this.state.count} /></div>;
+    return (
+      <div>
+        <Show count={this.state.count} />
+      </div>
+    );
   }
 }
 
